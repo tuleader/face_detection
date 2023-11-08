0a7891267b15c43624e96cab22bfca191eaacfc1
Tạ Ngọc Tú <tu.tn215503@sis.hust.edu.vn>
2023-11-08 16:53:21 +0700

    update

--- rcm.py
+++ rcm.py
@@ -3,23 +3,23 @@ import webbrowser
 
 def recommend_fashion(predicted_age):
     if predicted_age < 13:
-        return "Thời trang cho trẻ con", "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
+        return "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
     elif 13 <= predicted_age <= 19:
-        return "Thời trang dành cho tuổi teen", "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
+        return "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
     elif 20 <= predicted_age <= 29:
-        return "Phong cách urban chic và sáng tạo", "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
+        return "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
     elif 30 <= predicted_age <= 50:
-        return "Thời trang chín chắn và thanh lịch", "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
+        return "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
     else:
-        return "Phong cách classic và thoải mái", "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
+        return "https://shopee.vn/bitas_store?shopCollection=120740926#product_list","https://shopee.vn/obeahvn?shopCollection=158829356#product_list",'https://shopee.vn/benty.vn?shopCollection=246246813#product_list'
 
 def open_web(predicted_age):
-    fashion, shoe_url, shirt_url, hat_url = recommend_fashion(predicted_age)
+    shoe_url, shirt_url, hat_url = recommend_fashion(predicted_age)
 
     root = tk.Tk()
     root.title("Phân loại thời trang theo độ tuổi")
 
-    label_result = tk.Label(root, text=f"Phong cách thời trang cho độ tuổi {predicted_age}: {fashion}", bg="white", font=('Comic Sans MS', 15))
+    label_result = tk.Label(root, text=f"Hello, I wonder if you need any suggestions from us?", bg="white", font=('Comic Sans MS', 15))
     label_result.pack(padx=20, pady=20)
 
     def open_web_page(url):
